128da2b185449af8fa454aa3d83c1f672c594406
Quan_Dang <dangvietquan94@gmail.com>
2023-10-31 02:12:18 +0700

    'u'

--- src/boot/constant.js
+++ src/boot/constant.js
@@ -1,5 +1,5 @@
 import { boot } from "quasar/wrappers";
 
 export default boot(({ app }) => {
-    app.config.globalProperties.ip = 'http://localhost:80';
+    app.config.globalProperties.$ip = 'https://4c93-2a09-bac5-d45d-2c8-00-47-2f2.ngrok-free.app';
 });
