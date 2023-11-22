68412a36d90d8e40fab26682d27d1606819868d5
Dang Viet Quan <quandang@Dangs-MacBook-Air.local>
2023-11-22 13:38:44 +0700

    u

--- src/components/ModelsManager.vue
+++ src/components/ModelsManager.vue
@@ -1,30 +1,100 @@
 <template>
   <div>
-    <q-select
-      style="width: 200px"
-      filled
-      stack-label
-      dense
-      v-model="ttmId"
-      :options="ttmOptions"
-      label="Text to Mels"
-      emit-value
-      map-options
-      class="q-mb-sm"
-    />
-    <q-select
-      style="width: 200px"
-      filled
-      stack-label
-      dense
-      v-model="vocoderId"
-      :options="vocoderOptions"
-      label="Vocoder"
-      emit-value
-      map-options
-      class="q-mb-sm"
-    />
-    <q-btn label="Select" color="teal" @click="SelectModel()"></q-btn>
+    <div class="row">
+      <div class="col">
+        <q-select
+          style="width: 200px"
+          filled
+          stack-label
+          dense
+          v-model="ttmId"
+          :options="ttmOptions"
+          label="Text to Mels"
+          emit-value
+          map-options
+          class="q-mb-sm"
+        />
+        <q-select
+          style="width: 200px"
+          filled
+          stack-label
+          dense
+          v-model="vocoderId"
+          :options="vocoderOptions"
+          label="Vocoder"
+          emit-value
+          map-options
+          class="q-mb-sm"
+        />
+        <q-btn label="Select" color="teal" @click="SelectModel()"></q-btn>
+      </div>
+      <div class="col">
+        <div class="flex">
+          <q-select
+            style="width: 200px"
+            filled
+            stack-label
+            dense
+            v-model="algorithmId"
+            :options="algorithmOptions"
+            label="Select Algorithm"
+            emit-value
+            map-options
+            class="q-mb-sm"
+          />
+          <q-select
+            class="q-ml-sm"
+            filled
+            stack-label
+            dense
+            style="width: 200px"
+            v-model="datasetId"
+            :options="datasetOptions"
+            label="Select Dataset"
+            emit-value
+            map-options
+          />
+        </div>
+        <q-input
+          class="q-mb-sm"
+          style="width: 240px"
+          label="Enter model name"
+          type="text"
+          dense
+          v-model="modelName"
+        />
+        <q-btn label="Train" color="teal" @click="Train()"></q-btn>
+      </div>
+      <!-- <div class="col">
+        <div class="flex">
+          <q-select
+            style="width: 200px"
+            filled
+            stack-label
+            dense
+            v-model="ttmId"
+            :options="ttmOptions"
+            label="Select TTM Model"
+            emit-value
+            map-options
+            class="q-mb-sm"
+          />
+          <q-select
+            class="q-ml-sm"
+            filled
+            stack-label
+            dense
+            style="width: 200px"
+            v-model="datasetId"
+            :options="datasetOptions"
+            label="Select Dataset"
+            emit-value
+            map-options
+          />
+        </div>
+        <q-btn label="ReTrain" color="teal" @click="ReTrain()"></q-btn>
+      </div> -->
+    </div>
   </div>
 </template>
 <script>
@@ -46,6 +116,31 @@ export default defineComponent({
         method: "post",
         maxBodyLength: Infinity,
         url: `${this.ip}/model`,
+        headers: {
+          ...(data.getHeaders
+            ? data.getHeaders()
+            : { "Content-Type": "multipart/form-data" }),
+        },
+        data: data,
+      };
+
+      axios
+        .request(config)
+        .then((response) => {})
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    ReTrain() {},
+    Train() {
+      let data = new FormData();
+      data.append("algorithmId", this.algorithmId);
+      data.append("datasetId", this.datasetId);
+      data.append("modelName", this.modelName);
+      let config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: `${this.ip}/train`,
         headers: {
           ...(data.getHeaders
             ? data.getHeaders()
@@ -109,10 +204,60 @@ export default defineComponent({
           console.log(error);
         });
     },
+    fetchDataset() {
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: `${this.ip}/datasets`,
+        headers: {},
+      };
+
+      axios
+        .request(config)
+        .then(async (response) => {
+          this.datasets = response.data;
+          this.datasetOptions = [];
+          for (var dataset of this.datasets) {
+            this.datasetOptions.push({
+              label: dataset.name,
+              value: dataset.id,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    fetchAlgorithm() {
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: `${this.ip}/algorithms`,
+        headers: {},
+      };
+
+      axios
+        .request(config)
+        .then(async (response) => {
+          this.algorithms = response.data;
+          this.algorithmOptions = [];
+          for (var algorithm of this.algorithms) {
+            this.algorithmOptions.push({
+              label: algorithm.name,
+              value: algorithm.id,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   beforeMount() {
     this.fetchTtm();
     this.fetchVocoder();
+    this.fetchAlgorithm();
+    this.fetchDataset();
   },
   data() {
     return {
@@ -122,6 +267,13 @@ export default defineComponent({
       ttmOptions: [],
       vocoders: [],
       vocoderOptions: [],
+      datasets: [],
+      datasetOptions: [],
+      algorithms: [],
+      algorithmOptions: [],
+      modelName: "",
+      algorithmId: null,
+      datasetId: null,
       ip: this.$ip,
     };
   },
